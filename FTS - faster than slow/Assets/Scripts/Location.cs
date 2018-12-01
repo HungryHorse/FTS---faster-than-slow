d0bfcbffafd68b3588427a698d15f77cec11a9bd
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-12-01 17:15:14 +0000

    Events

--- FTS - faster than slow/Assets/Scripts/Location.cs
+++ FTS - faster than slow/Assets/Scripts/Location.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Location
+public class Location : MonoBehaviour
 {
     public string name;
-    public TextAsset[] fileNames;
-    public float eventChance;
+    public Event[] events;
+
+    private void Start()
+    {
+        events[0].SetText();
+    }
 }
