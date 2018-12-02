bf18b0ba9631a8ca3319cd5771de37cd43ca0b15
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-12-02 22:37:31 +0000

    StartScreen

---
+++ FTS - faster than slow/Assets/Scripts/AfterOpening.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AfterOpening : MonoBehaviour {
+
+    public Sprite titleScreen;
+    public Image curImage;
+    public GameObject button1;
+    public GameObject button2;
+
+    private void Awake()
+    {
+        button1.SetActive(false);
+        button2.SetActive(false);
+    }
+
+    public void AfterOpeningMethod()
+    {
+        Debug.Log("Hit");
+        button1.SetActive(true);
+        button2.SetActive(true);
+    }
+}
