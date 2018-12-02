7e69530408ba9761e9e65d37b49d80bae51f8f21
avebryhv <33032139+avebryhv@users.noreply.github.com>
2018-12-02 16:55:33 +0000

    p

--- FTS - faster than slow/Assets/Scripts/WarpIn.cs
+++ FTS - faster than slow/Assets/Scripts/WarpIn.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class WarpIn : MonoBehaviour {
 
     public LocationForShipInWorld spaceship;
+    public AudioClip warpSound;
 
     private void Awake()
     {
         GetComponent<Animator>().Play("WarpIn");
+        GameObject.FindGameObjectWithTag("SoundPlayer").GetComponent<SoundPlayer>().PlaySound(warpSound);
     }
 
     public void WarpedIn()
