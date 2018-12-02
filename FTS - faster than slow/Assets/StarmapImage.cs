12c1e8efddac285ea19bec5281d5e9b17500b4e8
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-12-02 23:42:54 +0000

    Starmap background

---
+++ FTS - faster than slow/Assets/StarmapImage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarmapImage : MonoBehaviour {
+
+    public GameObject starMap;
+    public GameObject starMapImage;
+	// Update is called once per frame
+	void Update ()
+    {
+        starMapImage.SetActive(starMap.activeInHierarchy);
+	}
+}
